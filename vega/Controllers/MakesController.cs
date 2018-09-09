94e53ec2b60c62df12686e072e57803cffce3b85
minkyawzin <minkz14@gmail.com>
2018-09-09 19:19:19 +0800

    20180909 07:20

---
+++ vega/Controllers/MakesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Models;
+using vega.Persistance;
+using vega.Resources;
+
+namespace vega.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class MakesController : ControllerBase
+    {
+        private readonly VegaDBContext context;
+        private readonly IMapper mapper;
+
+        //dependency injection
+        public MakesController(VegaDBContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<MakeResource>> GetMakes()
+        {
+            var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+
+
+            return mapper.Map<List<Make>, List<MakeResource>>(makes);
+        }
+    }
+}
--- vega/Startup.cs
+++ vega/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +27,9 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddAutoMapper();
+
             services.AddDbContext<VegaDBContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Default"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -37,7 +41,8 @@ namespace vega
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(builder =>
+             builder.WithOrigins("http://localhost:4200"));
             app.UseMvc();
         }
     }
